268059caa085122fef8d9327f082b6b90cab7184
rddpgn <rddpgn@gmail.com>
2020-01-19 17:35:14 +0300

    Прикрутили scss

---
+++ react-notes/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header.js';
+import './App.scss';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default App;
--- react-notes/src/index.js
+++ react-notes/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (module.hot) {
+    module.hot.accept('./components/App/App.js', () => {
+      const NextApp = require('./components/App/App.js').default
+      ReactDOM.render(
+        <NextApp />,
+        document.getElementById('root')
+      )
+    })
+}
