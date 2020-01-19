60613a9c2a907516d078c5af9e83831e88bfca5e
rddpgn <rddpgn@gmail.com>
2020-01-19 16:48:38 +0300

    Инициализация проекта

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import App from "./app.component";
-let tmp = document.getElementById('root');
-console.log(tmp);
-ReactDOM.render(<App />, tmp);
