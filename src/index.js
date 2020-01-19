c9964653fa374ed30daba952e47440baa0955fc7
rddpgn <rddpgn@gmail.com>
2020-01-19 12:30:07 +0300

    Инициализация проекта реакт

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./app.component";
+let tmp = document.getElementById('root');
+console.log(tmp);
+ReactDOM.render(<App />, tmp);
